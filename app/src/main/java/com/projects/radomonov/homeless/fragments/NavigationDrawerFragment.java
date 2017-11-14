a4ada69cf131ce08059b86f6fd5e09eacc454100
Dmitriy <dmitriystanev@mail.ru>
2017-11-14 19:28:32 +0200

    Requiring Storage permission, when its needed

--- app/src/main/java/com/projects/radomonov/homeless/fragments/NavigationDrawerFragment.java
+++ app/src/main/java/com/projects/radomonov/homeless/fragments/NavigationDrawerFragment.java
@@ -1,20 +1,26 @@
 package com.projects.radomonov.homeless.fragments;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.Image;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v4.widget.DrawerLayout;
@@ -53,6 +59,8 @@ import static java.lang.System.load;
  */
 
 public class NavigationDrawerFragment extends android.app.Fragment implements View.OnClickListener {
+
+    private static final int PERMISSION_REQUEST_CODE = 123;
 
     private ActionBarDrawerToggle mDrawerToggle;
     private DrawerLayout mDrawerLayout;
@@ -172,11 +180,16 @@ public class NavigationDrawerFragment extends android.app.Fragment implements Vi
                 break;
 
             case R.id.btn_create_offer :
-                fragmentTransaction = fragmentManager.beginTransaction();
-                CreateOfferFragment fragment = new CreateOfferFragment();
-                fragmentTransaction.replace(R.id.fragment_container_main,fragment,getResources().getString(R.string.create_offer_frag_tag));
-                fragmentTransaction.commit();
+                if(hasPermissions()) {
+                    fragmentTransaction = fragmentManager.beginTransaction();
+                    CreateOfferFragment fragment = new CreateOfferFragment();
+                    fragmentTransaction.replace(R.id.fragment_container_main,fragment,getResources().getString(R.string.create_offer_frag_tag));
+                    fragmentTransaction.commit();
+                } else {
+                    requestPermissionWithRationale();
+                }
                 break;
+
 
             case R.id.btn_my_offers :
                 fragmentTransaction = fragmentManager.beginTransaction();
@@ -193,10 +206,14 @@ public class NavigationDrawerFragment extends android.app.Fragment implements Vi
                 break;
 
             case R.id.img_edit_profile_drawer_frag :
-                fragmentTransaction = fragmentManager.beginTransaction();
-                SetupAccountFragment setupFrag = new SetupAccountFragment();
-                fragmentTransaction.replace(R.id.fragment_container_main, setupFrag, getResources().getString(R.string.setup_acc_frag_tag));
-                fragmentTransaction.commit();
+                if(hasPermissions()) {
+                    fragmentTransaction = fragmentManager.beginTransaction();
+                    SetupAccountFragment setupFrag = new SetupAccountFragment();
+                    fragmentTransaction.replace(R.id.fragment_container_main, setupFrag, getResources().getString(R.string.setup_acc_frag_tag));
+                    fragmentTransaction.commit();
+                } else {
+                    requestPermissionWithRationale();
+                }
                 break;
 
             default:  break;
@@ -220,6 +237,106 @@ public class NavigationDrawerFragment extends android.app.Fragment implements Vi
         btnMyOffers = view.findViewById(R.id.btn_my_offers);
         btnMyFavouriteOffers = view.findViewById(R.id.btn_my_favourite_offers);
         imgEditProfile = view.findViewById(R.id.img_edit_profile_drawer_frag);
+    }
+
+
+    //for requesting permissions
+    private boolean hasPermissions(){
+        int res = 0;
+        //string array of permissions,
+        String[] permissions = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE};
+
+        for (String perms : permissions){
+            res = getActivity().checkCallingOrSelfPermission(perms);
+            if (!(res == PackageManager.PERMISSION_GRANTED)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void requestPerms(){
+        String[] permissions = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE};
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            requestPermissions(permissions,PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        boolean allowed = true;
+
+        switch (requestCode){
+            case PERMISSION_REQUEST_CODE:
+
+                for (int res : grantResults){
+                    // if user granted all permissions.
+                    allowed = allowed && (res == PackageManager.PERMISSION_GRANTED);
+                }
+
+                break;
+            default:
+                // if user not granted permissions.
+                allowed = false;
+                break;
+        }
+
+        if (!allowed){
+            // we will give warning to user that they haven't granted permissions.
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)){
+                    Toast.makeText(getActivity(), "Storage Permissions denied.", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    showNoStoragePermissionSnackbar();
+                }
+            }
+        }
+
+    }
+
+    public void showNoStoragePermissionSnackbar() {
+        Snackbar.make(getActivity().findViewById(R.id.drawer_layout_main), "Storage permission isn't granted" , Snackbar.LENGTH_LONG)
+                .setAction("SETTINGS", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        openApplicationSettings();
+
+                        Toast.makeText(getActivity(),
+                                "Open Permissions and grant the Storage permission",
+                                Toast.LENGTH_LONG)
+                                .show();
+                    }
+                })
+                .show();
+    }
+
+    public void openApplicationSettings() {
+        Intent appSettingsIntent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                Uri.parse("package:" + getActivity().getPackageName()));
+        startActivityForResult(appSettingsIntent, PERMISSION_REQUEST_CODE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void requestPermissionWithRationale() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+                Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            final String message = "Storage permission is needed to show files count";
+            Snackbar.make(getActivity().findViewById(R.id.drawer_layout_main), message, Snackbar.LENGTH_LONG)
+                    .setAction("GRANT", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            requestPerms();
+                        }
+                    })
+                    .show();
+        } else {
+            requestPerms();
+        }
     }
 
 }
